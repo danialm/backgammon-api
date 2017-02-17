556503ed64febb63505d1d74e0cda53ab23796c3
Danial Moazen <dmoazen@fandango.com>
2017-02-17 14:30:21 -0800

    running pg manually

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170217193540) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "games", force: :cascade do |t|
     t.string   "name"
     t.text     "status"
@@ -25,8 +28,8 @@ ActiveRecord::Schema.define(version: 20170217193540) do
     t.boolean  "accepted",   default: false
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
-    t.index ["game_id"], name: "index_user_games_on_game_id"
-    t.index ["user_id"], name: "index_user_games_on_user_id"
+    t.index ["game_id"], name: "index_user_games_on_game_id", using: :btree
+    t.index ["user_id"], name: "index_user_games_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
