27f6b2bc42452ff3f44c897dbfecb0d2c5b49368
Danial Moazen <dmoazen@fandango.com>
2017-02-17 16:17:47 -0800

    change origins

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module BackgammonApi
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3001', 'https://danialm.github.io/backgammon/'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :put, :options]
       end
     end
