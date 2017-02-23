2b7b3921af1eb2e3f242cf5b50bf7bb0f1dfa8bd
Danial Moazen <dmoazen@fandango.com>
2017-02-22 17:28:18 -0800

    after @ in case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,33 @@ class User < ApplicationRecord
   has_many :user_games, dependent: :destroy
   has_many :games, through: :user_games
 
+  before_validation :normalize_email
+
   validates :email, presence: true
   validates :email, uniqueness: true
   validates :email, format: {
     with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   }
 
+  def self.from_token_request request
+    email = request.params["auth"] && request.params["auth"]["email"]
+    email = self.email_to_lowecase(email)
+    self.find_by email: email
+  end
+
   def to_hash
-    {
-      name: try(:name) || 'No Name',
-      email: email
-    }
+    { name: try(:name) || 'No Name', email: email }
+  end
+
+  protected
+
+  def normalize_email
+    email_array = self.email.split('@')
+    self.email = self.class.email_to_lowecase(email)
+  end
+
+  def self.email_to_lowecase(email)
+    email_array = email.split('@')
+    [email_array[0], email_array[1].downcase].join('@')
   end
 end
